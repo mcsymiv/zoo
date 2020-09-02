ad7335fd85b7a039617bf60afa2f67b96baa7bc5
Memorik <memorik28@gmail.com>
2020-09-02 18:48:24 +0300

    Add metod in clas Zoo

--- ZooApp/ZooApp/Zoo.cs
+++ ZooApp/ZooApp/Zoo.cs
@@ -4,8 +4,19 @@ using System.Text;
 
 namespace ZooApp
 {
-    class Zoo
+    public class Zoo
     {
+        public int id;
+        public string name;
+        public bool isScary;
+        public int priceTicket;
 
+        public Animal(int id, string name, bool isScary, int priceTicket)
+        {
+            this.id = id;
+            this.name = name;
+            this.isScary = isScary;
+            this.priceTicket = priceTicket;
+        }
     }
 }
