3150e6dc642b389966595d68df3421b4f9e48a59
mcs <s.mcsymiv@gmail.com>
2020-09-02 13:04:08 +0300

    Add app folder

---
+++ ZooApp/ZooApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZooApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is zoo app");
+        }
+    }
+}
