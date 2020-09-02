c545ba9f857358e6b04ffed9b1aec0e2ba2ec4f7
Anna Demchuk <anidemchuk@gmail.com>
2020-09-02 14:10:32 +0300

    Class add fiels

--- ZooApp/ZooApp/Animal.cs
+++ ZooApp/ZooApp/Animal.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace ZooApp
 {
-  public  class Animal
+    public class Animal
     {
+        public int id;
+        public string name;
+        public bool isScary;
+        public int priceTicket;
+
+        public Animal(int id, string name, bool isScary, int priceTicket)
+        {
+            this.id = id;
+            this.name = name;
+            this.isScary = isScary;
+            this.priceTicket = priceTicket;
+        }
     }
 }
